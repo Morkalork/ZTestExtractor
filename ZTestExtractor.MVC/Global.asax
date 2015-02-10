645257bea0d908ca4a7f6cc584c3c9204b0e293d
Magnus Ferm <maffelu@maffelu.net>
2015-02-10 19:48:34 +0100

    Added a presentation layer under the solution folder 'UI'. An MVC project will make it easy to generate reports and would make sense to use in order to distribute the usage of this product to multiple users.

---
+++ ZTestExtractor.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZTestExtractor.MVC.MvcApplication" Language="C#" %>
---
+++ ZTestExtractor.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZTestExtractor.MVC.Startup))]
+namespace ZTestExtractor.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
