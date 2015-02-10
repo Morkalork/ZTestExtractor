cd1ae82da227b3ecec2f70fd9d07dcf57f0369a4
Magnus Ferm <maffelu@maffelu.net>
2015-02-10 19:53:33 +0100

    Removed the MVC project added through visual studio. Horrible amounts of stuff that I did not want.

--- ZTestExtractor.MVC/App_Start/IdentityConfig.cs
+++ ZTestExtractor.MVC/App_Start/IdentityConfig.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using ZTestExtractor.MVC.Models;
 
 namespace ZTestExtractor.MVC
 {
--- ZTestExtractor.MVC/App_Start/Startup.Auth.cs
+++ ZTestExtractor.MVC/App_Start/Startup.Auth.cs
@@ -5,7 +5,6 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
-using ZTestExtractor.MVC.Models;
 
 namespace ZTestExtractor.MVC
 {
