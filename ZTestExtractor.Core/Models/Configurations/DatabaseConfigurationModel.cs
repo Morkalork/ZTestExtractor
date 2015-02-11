55e51129d3c7d978da99b8d649788431ad4cfda4
Magnus Ferm <maffelu@maffelu.net>
2015-02-11 17:15:21 +0100

    Added a Database configuration model as well as a test project for businesa

---
+++ ZTestExtractor.Core/Models/Configurations/DatabaseConfigurationModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTestExtractor.Core.Models.Configurations
+{
+    public class DatabaseConfigurationModel
+    {
+        public string ServerName { get; set; }
+
+        public string DatabaseName { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public DatabaseSystems DatabaseSystem { get; set; }
+    }
+}
---
+++ ZTestExtractor.Core/Models/Configurations/DatabaseSystems.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTestExtractor.Core.Models.Configurations
+{
+    public enum DatabaseSystems
+    {
+        Unknown = 0,
+        MySql = 1
+    }
+}
