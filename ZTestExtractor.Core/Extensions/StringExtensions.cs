ed895aaab00ee3359e7bf300d62aed6490283e68
Magnus Ferm <maffelu@maffelu.net>
2015-05-03 14:02:03 +0200

    Added a resource project as well as some basic translation into the test index view. Also added a string.format extension used in the test index view.

---
+++ ZTestExtractor.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTestExtractor.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string F(this string str, params object[] args)
+        {
+            return string.Format(str, args);
+        }
+    }
+}
